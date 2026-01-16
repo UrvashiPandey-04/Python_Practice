c5eea370b6d67eefa1b6714112a9ca6eaffb3874
Urvashi Pandey <pandeyurvashi64@gmail.com>
2026-01-16 17:32:06 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,152 @@
+# print("hello world")
+
+#Input-output
+# name=input("enter your name")
+# print("welcome",name)
+
+# name="Urvashi"
+# age=20
+# country="India"
+# print(name,age,country)
+
+#enter multiple values
+# x,y=input("enter your full name").split()
+# print("my first name is ",x)
+# print("my last name is ",y)
+
+# x=int(input("enter your age:"))
+# print(x)
+
+# y=float(input("enter your salary:"))
+# print(y)
+
+#type of variables
+# a=25
+# b=25.5
+# c="urvashi"
+# d=[25,10,45]
+# e=("a","b","c")
+# f={"a":1,"b":2}
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+
+#multiple assignment to variables
+# a=b=c=1
+# print(a,b,c)
+
+#type casting a variable
+# a=10
+# b=str(a)
+# print(type(a))
+# print(type(b))
+
+#deleting a variable
+# x=85
+# del(x)
+# print(x)- will result in name error
+
+#swapping of variables
+# a,b=5,10
+# print("before swapping a:",a)
+# print("before swapping a:",b)
+# a,b=b,a
+# print("after swappping",a)
+# print("after swapping",b)
+
+#counting characters in a string
+# name="urvashi"
+# print("length is:",len(name))
+
+#arithmetic operator
+# a=50
+# b=3
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+#relational or comparison operator
+# print(a>b)
+# print(a<b)
+# print(a==b)
+# print(a!=b)
+# print(a>=b)
+# print(a<=b)
+
+#logical operator
+# a=True
+# b=False
+# print(a and b)
+# print(a or b)
+# print(not a)
+
+#bitwise operator
+# a=8
+# b=2
+# print(a & b)
+# print(a | b)
+# print(~a)
+# print(a^b)
+# print(a<<b)
+# print(a>>b)
+
+#assignment operator
+# a=100
+# b=a
+# print(b)
+# b+=a
+# print(b)
+# b-=a
+# print(b)
+# b*=a
+# print(b)
+
+#Identity operator
+# a=10
+# b=10
+# print(a is b)
+# print(a is not b)
+
+#membership operator
+# a=[10,20,30,40,50]
+# print(10 in a)
+# print(500 not in a)
+
+# a=5
+# b=a
+# print(id(a))
+# print(id(b))
+# print(a==b)
+# print(a is b)
+
+# a=10
+# b=10
+# c=a
+# print(id(c))
+# print(id(b))
+# print(c==b)
+# print(c is b)
+
+#ternary operator
+# a,b=10,20
+# min=a if a>b else b
+# print(min)
+
+#== and is operator
+# a=[1,2,3]
+# b=[1,2,3]
+# print(a==b)
+# print(a is b)
+
+#keywords - 35 in python
+# import keyword
+# print(keyword.kwlist)
+
+
