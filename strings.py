3e07ecbc20b684697e47dd9fc75bb97bf0b3ac12
Urvashi Pandey <pandeyurvashi64@gmail.com>
2026-01-18 23:42:39 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,95 @@
+# a='a'
+# print(a)
+# print(type(a))
+
+# a="a"
+# print(a)
+# print(type(a))
+
+#multiline string
+# a='''
+#     hello
+#     world
+#     '''
+# print(a)
+# print(type(a))
+
+#indexing
+# s="Hello World"
+# print(s[0])
+# print(s[6])
+# print(s[-7])
+
+#slicing
+# print(s[4:9])
+# print(s[0::2])
+# print(s[-1:])
+
+#string iteration
+# s="helloworld"
+# for i in s:
+#     print(i)
+
+#string immutability
+# s="hello"
+# n=s+"world" - this creates new string
+# print(n)
+
+#deleting string
+# s="hello world"
+# del s
+# print(s)- will result in error
+
+#updating a string
+# s="hello"
+# n=s+" world"
+# print(n)
+
+# n=s.replace("hello","hello world")
+# print(n)
+
+#string methods
+# s="python is a programming language "
+# print(len(s))
+# print(s.upper())
+# print(s.lower())
+# print(s.title())
+# print(s.capitalize())
+# print(s.swapcase())
+# print(s.strip())
+# print(s.lstrip())
+# print(s.rstrip())
+# print(s.find("python"))
+# print(s.find("p",2,9))
+# print(s.index("is"))
+# print(s.index("of")) - will result in value error
+# print(s.count("p"))
+# print(s.count("a"))
+# l=s.split()
+# for i in l:
+#     print(i)
+
+# t=("a","b","c")
+# l='-'.join(t)
+# print(l)
+
+# print(s.startswith("python"))
+# print(s.endswith("python"))
+
+# a="a"
+# b="b"
+# print(a+b)
+# print(a*3)
+
+#format string
+name="urvashi"
+age="20"
+# print("my name is {} and i am {} years old".format(name,age))
+# print(f"my name is {name} and i am {age} years old")
+
+#membership operator
+# print("a" in name)
+
+
+
+
