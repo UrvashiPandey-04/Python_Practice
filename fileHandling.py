be494993564d344323e865d6b90d9df4244ca8e6
Urvashi Pandey <pandeyurvashi64@gmail.com>
2026-03-01 16:25:29 +0530

    file handling practice

--- fileHandling.py
+++ fileHandling.py
@@ -11,7 +11,62 @@ f=open('sample.txt','a')
 f.write('hope you are doing okay!')
 f.close()
 
-l=['hello\n','how are you\n','i am fine']
-f=open('sample.txt','a')
-f.writelines(l)
-f.close()
+# l=['hello\n','how are you\n','i am fine']
+# f=open('sample.txt','a')
+# f.writelines(l)
+# f.close()
+
+# f=open('sample.txt','r')
+# s=f.read(10)
+# print(s)
+# f.close()
+
+# f=open('sample.txt','r')
+# print(f.readline(),end='')
+# print(f.readline(),end='')
+# f.close()
+
+# reading entirely using readline
+# f=open('sample.txt','r')
+# while True:
+#     data=f.readline()
+#     if data=="":
+#         break
+#     else:
+#         print(data,end=" ")
+
+#using with
+# with open('sample.txt','w') as f:
+#     f.write('Hello World ')
+
+# with open('sample.txt','r') as f:
+#     print(f.read())
+
+#seek and tell
+# with open('sample.txt','r') as f:
+#     print(f.read(10))
+#     print(f.tell())
+#     f.seek(0)
+#     print(f.read(10))
+#     print(f.tell())
+
+# with open('sample.txt','w') as f:
+#     print(f.write(10))- error as it is int type
+
+#serialization and deserialization
+import json
+# l=[1,2,3,4]
+# with open('demo.json','w') as f:
+#     json.dump(l,f)
+
+d={
+    'name':'riya',
+    'age':35,
+    'gender':'female'}
+with open('demo.json','w') as f:
+    json.dump(d,f)
+
+with open('demo.json','r') as f:
+    d=json.load(f)
+    print(d)
+    print(type(d))
